fd2c86ad23c0d4b43307e52edc68ae62256e4c3c
AldaAB <aldahir.aburto@onexo.mx>
2024-08-13 17:07:55 -0600

    first commit

---
+++ src/FirstApp.jsx
@@ -0,0 +1,22 @@
+
+// const newMessage = {
+//     message: 'HolaMundo',
+//     title: 'Fernando'
+// };
+
+// const mulPor5 = (num) => {
+//     return num * 5
+// }
+
+export const FirstApp = ( {title, subTitle} ) => {
+
+    // console.log(props)
+
+    return (
+        <>
+            <h1>  { title }  </h1>
+            <p> {subTitle} </p>
+        </>
+    )
+}
+
---
+++ src/HelloWordApp.jsx
@@ -0,0 +1,10 @@
+// export function App() {
+//     // document.createElement
+//     return <h1>Hola mundo!!</h1>;
+// }
+
+export const HelloWordApp = () => {
+    return(
+        <h1>HelloWordApp</h1>
+    );
+}
